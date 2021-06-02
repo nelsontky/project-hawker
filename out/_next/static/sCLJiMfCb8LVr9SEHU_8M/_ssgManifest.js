a9f27244711d1c504fd62363e83a9280f04a0e4d
nelsontky <nelsoN@ccb.wtf>
2021-06-02 06:17:49 +0000

    Deploy Next.js to gh-pages

---
+++ out/_next/static/sCLJiMfCb8LVr9SEHU_8M/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fsearch","\u002Fall\u002F[pageNumber]","\u002F[locationSlug]","\u002F[locationSlug]\u002F[stallSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
